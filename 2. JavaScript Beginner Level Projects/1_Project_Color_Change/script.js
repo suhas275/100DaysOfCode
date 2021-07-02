b0e39e4e3b29cce6d08c16a1f3388494a51a89a7
suhas275 <67008179+suhas275@users.noreply.github.com>
2021-07-02 22:46:32 +0530

    version 1.0

---
+++ 2. JavaScript Beginner Level Projects/1_Project_Color_Change/script.js
@@ -0,0 +1,23 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+const getBGColor = (selectedElement) =>{
+    return window.getComputedStyle(selectedElement).backgroundColor;
+};
+
+const magicColorChanger = (element , color) =>{
+    return element.addEventListener("mouseenter",() =>{
+        center.style.background = color;
+    });
+};
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(pink, getBGColor(pink));
---
+++ 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
@@ -0,0 +1,168 @@
+//to attach JS File in HTML file declare 
+//  🎯<script src="./script.js"></script> declare this after all html element below so that HTML elements can loadfirst
+
+
+//this code output is  shown in the console
+
+// console.log("hello world");
+
+
+
+//🚀1.how to name a variable
+//In program code =>🔥 a.camelCase and b.PascalCase of writing varialble is practiced more often.
+
+// var courseName ="React JS";
+// console.log(courseName);
+
+// const uid="abc123";
+
+// var fullName="MadaraUchiha";
+// var email="Madara@gmail.com";
+// var password="12345";
+// var confirmPassword = "12345";
+// var courseCount = 0;
+// var isLoggedInFromGoogle = false;
+
+
+// console.log(uid);
+// console.log("Full Name is:"+fullName);
+// console.log(email);
+// //this is JS good practice for declaration
+// console.log(`      
+//     With Unique ID:${uid}
+//     User is : ${fullName}
+//     and his email is : ${email}
+//     and users the password : ${password}
+// `);
+
+
+
+//🚀2. Operators
+
+// var sellingPrice =199;
+// var listingPrice =799;
+
+// var discountPercent = ((listingPrice - sellingPrice)/listingPrice)*100;
+
+// console.log(`Discount percentage is :${discountPercent}`);
+
+// displayDiscountPercentage = Math.round(discountPercent);
+
+// console.log(displayDiscountPercentage + "% off");
+
+// var result = listingPrice > sellingPrice;
+
+// console.log(typeof result);
+
+
+//🚀3. Conditional Statement 
+
+// var temperature;
+
+// temperature=49;
+
+// if(temperature<20){
+//     console.log("it's cold outsode");
+// }
+
+// if(temperature < 30){
+//     console.log("It's moderate outside");
+// }else {
+//     console.log("It's HOT outside");
+// }
+
+//Ex=2🔥
+// var email =true;
+// var facebook = false;
+// var google = true;
+
+// if(email || facebook || google){
+//     console.log("login Success");
+// }
+
+// //Ex=3 🔥Switch case
+// var user = "admin";
+
+// switch (user) {
+//     case "admin":
+//         console.log("you get full access");
+//         break;
+//     case "subadmin":
+//         console.log("gets access to create/delete course");
+//         break;
+//     default:
+//         break;
+// }
+
+//$$$$$$$$$$$$$$$$$$$$$$    Functions   $$$$$$$$$$$$$$$$$$$$$$$$ 
+
+
+// //Ex:1🔥
+// function sayHello(name) {
+//     console.log("hello there, Madara");
+//     console.log(`Hello there ,${name}.How are ypu`);
+// }
+
+// sayHello("MadaraUchiha");
+// sayHello("Madara");
+
+//$$$$$$$$$$$$$$$$$$$$$$    Arrays and Loops   $$$$$$$$$$$$$$$$$$$$$$$$ 
+
+// // //Ex:1🔥
+// var countries = ["India", "USA", "Japan", "Turkey"];
+
+// var states = new Array("Rajasthan", "Delhi", "Mumbai", "Assam");
+
+// console.log(countries[3]);
+// console.log(states[3]);
+
+// // 🚀 Objects
+// var user = {
+//     firstName: "Madara",
+//     lastName: "Uchiha",
+//     role: "Admin",
+//     loginCount: 32,
+// }
+
+// console.log(user.firstName);
+// console.log(user["lastName"]);
+
+// console.log(user.loginCount);
+// user.loginCount = 44;
+// console.log(user.loginCount);
+// console.table(user);
+
+// // 🚀 forEach
+
+// const myStates =[
+//     "Rajasthan",
+//     "Delhi",
+//     "Assam",
+// ];
+
+// myStates.forEach((s) =>(console.log(s)));
+
+// const names =["Youtube", "facebook", "instagram", ];
+// const symbols ={
+//     yt:"youtube",
+//     ig:"Instragram",
+
+// };
+
+// for(const n in symbols){
+//     console.log(`Key is:${n}`);
+// }
+
+//$$$$$$$$$$$$$$$$$$$$$$    Clousers  $$$$$$$$$$$$$$$$$$$$$$$$ 
+
+// function doAddition(x) {
+//     return function (y){
+//         return x+y;
+//     };
+// }
+
+// var add5 = doAddition(4);
+// console.log(add5(5));
+
+
+
