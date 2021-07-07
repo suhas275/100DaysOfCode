85d188ec1278275cd46be519c61212f479c20009
suhas275 <67008179+suhas275@users.noreply.github.com>
2021-07-07 23:17:48 +0530

    version 1.1

--- 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
+++ 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
@@ -165,4 +165,56 @@
 // console.log(add5(5));
 
 
+// // 🚀 This Keyword
+// console.log(this);
 
+// var user = {
+//   firstName: "Hitesh",
+//   courseCount: 4,
+//   getCourseCount: function () {
+//     console.log("LINE 7", this);
+//     function sayHello() {
+//       console.log("Hello");
+//       console.log("LINE 10", this);
+//     }
+//     sayHello();
+//   },
+// };
+
+// user.getCourseCount();
+
+// // 🚀
+// const uno = () => {
+//   return "I am One";
+// };
+
+// // const dos = () => {
+// //   setTimeout(() => {
+// //     return "I am two";
+// //   }, 3000);
+// // };
+
+// const dos = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("I am two");
+//     }, 3000);
+//   });
+// };
+
+// const tres = () => {
+//   return "I am Three";
+// };
+
+// const callMe = async () => {
+//   let valOne = uno();
+//   console.log(valOne);
+
+//   let valTwo = await dos();
+//   console.log(valTwo);
+
+//   let valThree = tres();
+//   console.log(valThree);
+// };
+
+// callMe();
