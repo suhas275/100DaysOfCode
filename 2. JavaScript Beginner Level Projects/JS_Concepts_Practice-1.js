213f223e80eaf15ab740cc709928d0edfa0aca72
Suhas Gowda <67008179+suhas275@users.noreply.github.com>
2021-07-10 23:18:46 +0530

    version 1.2

--- 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
+++ 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
@@ -1,220 +1,231 @@
-//to attach JS File in HTML file declare 
-//  🎯<script src="./script.js"></script> declare this after all html element below so that HTML elements can loadfirst
+to attach JS File in HTML file declare 
+ 🎯<script src="./script.js"></script> declare this after all html element below so that HTML elements can loadfirst
 
 
-//this code output is  shown in the console
+this code output is  shown in the console
 
-// console.log("hello world");
-
-
-
-//🚀1.how to name a variable
-//In program code =>🔥 a.camelCase and b.PascalCase of writing varialble is practiced more often.
-
-// var courseName ="React JS";
-// console.log(courseName);
-
-// const uid="abc123";
-
-// var fullName="MadaraUchiha";
-// var email="Madara@gmail.com";
-// var password="12345";
-// var confirmPassword = "12345";
-// var courseCount = 0;
-// var isLoggedInFromGoogle = false;
-
-
-// console.log(uid);
-// console.log("Full Name is:"+fullName);
-// console.log(email);
-// //this is JS good practice for declaration
-// console.log(`      
-//     With Unique ID:${uid}
-//     User is : ${fullName}
-//     and his email is : ${email}
-//     and users the password : ${password}
-// `);
+console.log("hello world");
 
 
 
-//🚀2. Operators
+🚀1.how to name a variable
+In program code =>🔥 a.camelCase and b.PascalCase of writing varialble is practiced more often.
 
-// var sellingPrice =199;
-// var listingPrice =799;
+var courseName ="React JS";
+console.log(courseName);
 
-// var discountPercent = ((listingPrice - sellingPrice)/listingPrice)*100;
+const uid="abc123";
 
-// console.log(`Discount percentage is :${discountPercent}`);
-
-// displayDiscountPercentage = Math.round(discountPercent);
-
-// console.log(displayDiscountPercentage + "% off");
-
-// var result = listingPrice > sellingPrice;
-
-// console.log(typeof result);
+var fullName="MadaraUchiha";
+var email="Madara@gmail.com";
+var password="12345";
+var confirmPassword = "12345";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
 
 
-//🚀3. Conditional Statement 
+console.log(uid);
+console.log("Full Name is:"+fullName);
+console.log(email);
+//this is JS good practice for declaration
+console.log(`      
+    With Unique ID:${uid}
+    User is : ${fullName}
+    and his email is : ${email}
+    and users the password : ${password}
+`);
 
-// var temperature;
 
-// temperature=49;
 
-// if(temperature<20){
-//     console.log("it's cold outsode");
-// }
+🚀2. Operators
 
-// if(temperature < 30){
-//     console.log("It's moderate outside");
-// }else {
-//     console.log("It's HOT outside");
-// }
+var sellingPrice =199;
+var listingPrice =799;
 
-//Ex=2🔥
-// var email =true;
-// var facebook = false;
-// var google = true;
+var discountPercent = ((listingPrice - sellingPrice)/listingPrice)*100;
 
-// if(email || facebook || google){
-//     console.log("login Success");
-// }
+console.log(`Discount percentage is :${discountPercent}`);
 
-// //Ex=3 🔥Switch case
-// var user = "admin";
+displayDiscountPercentage = Math.round(discountPercent);
 
-// switch (user) {
-//     case "admin":
-//         console.log("you get full access");
-//         break;
-//     case "subadmin":
-//         console.log("gets access to create/delete course");
-//         break;
-//     default:
-//         break;
-// }
+console.log(displayDiscountPercentage + "% off");
 
-//$$$$$$$$$$$$$$$$$$$$$$    Functions   $$$$$$$$$$$$$$$$$$$$$$$$ 
+var result = listingPrice > sellingPrice;
 
+console.log(typeof result);
+
+
+🚀3. Conditional Statement 
+
+var temperature;
+
+temperature=49;
+
+if(temperature<20){
+    console.log("it's cold outsode");
+}
+
+if(temperature < 30){
+    console.log("It's moderate outside");
+}else {
+    console.log("It's HOT outside");
+}
+
+Ex=2🔥
+var email =true;
+var facebook = false;
+var google = true;
+
+if(email || facebook || google){
+    console.log("login Success");
+}
+
+//Ex=3 🔥Switch case
+var user = "admin";
+
+switch (user) {
+    case "admin":
+        console.log("you get full access");
+        break;
+    case "subadmin":
+        console.log("gets access to create/delete course");
+        break;
+    default:
+        break;
+}
+
+$$$$$$$$$$$$$$$$$$$$$$    Functions   $$$$$$$$$$$$$$$$$$$$$$$$ 
+
+
+//Ex:1🔥
+function sayHello(name) {
+    console.log("hello there, Madara");
+    console.log(`Hello there ,${name}.How are ypu`);
+}
+
+sayHello("MadaraUchiha");
+sayHello("Madara");
+
+$$$$$$$$$$$$$$$$$$$$$$    Arrays and Loops   $$$$$$$$$$$$$$$$$$$$$$$$ 
 
 // //Ex:1🔥
-// function sayHello(name) {
-//     console.log("hello there, Madara");
-//     console.log(`Hello there ,${name}.How are ypu`);
-// }
+var countries = ["India", "USA", "Japan", "Turkey"];
 
-// sayHello("MadaraUchiha");
-// sayHello("Madara");
+var states = new Array("Rajasthan", "Delhi", "Mumbai", "Assam");
 
-//$$$$$$$$$$$$$$$$$$$$$$    Arrays and Loops   $$$$$$$$$$$$$$$$$$$$$$$$ 
+console.log(countries[3]);
+console.log(states[3]);
 
-// // //Ex:1🔥
-// var countries = ["India", "USA", "Japan", "Turkey"];
+// 🚀 Objects
+var user = {
+    firstName: "Madara",
+    lastName: "Uchiha",
+    role: "Admin",
+    loginCount: 32,
+}
 
-// var states = new Array("Rajasthan", "Delhi", "Mumbai", "Assam");
+console.log(user.firstName);
+console.log(user["lastName"]);
 
-// console.log(countries[3]);
-// console.log(states[3]);
+console.log(user.loginCount);
+user.loginCount = 44;
+console.log(user.loginCount);
+console.table(user);
 
-// // 🚀 Objects
-// var user = {
-//     firstName: "Madara",
-//     lastName: "Uchiha",
-//     role: "Admin",
-//     loginCount: 32,
-// }
+// 🚀 forEach
 
-// console.log(user.firstName);
-// console.log(user["lastName"]);
+const myStates =[
+    "Rajasthan",
+    "Delhi",
+    "Assam",
+];
 
-// console.log(user.loginCount);
-// user.loginCount = 44;
-// console.log(user.loginCount);
-// console.table(user);
+myStates.forEach((s) =>(console.log(s)));
 
-// // 🚀 forEach
+const names =["Youtube", "facebook", "instagram", ];
+const symbols ={
+    yt:"youtube",
+    ig:"Instragram",
 
-// const myStates =[
-//     "Rajasthan",
-//     "Delhi",
-//     "Assam",
-// ];
+};
 
-// myStates.forEach((s) =>(console.log(s)));
+for(const n in symbols){
+    console.log(`Key is:${n}`);
+}
 
-// const names =["Youtube", "facebook", "instagram", ];
-// const symbols ={
-//     yt:"youtube",
-//     ig:"Instragram",
+$$$$$$$$$$$$$$$$$$$$$$    Clousers  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-// };
+function doAddition(x) {
+    return function (y){
+        return x+y;
+    };
+}
 
-// for(const n in symbols){
-//     console.log(`Key is:${n}`);
-// }
-
-//$$$$$$$$$$$$$$$$$$$$$$    Clousers  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-// function doAddition(x) {
-//     return function (y){
-//         return x+y;
-//     };
-// }
-
-// var add5 = doAddition(4);
-// console.log(add5(5));
+var add5 = doAddition(4);
+console.log(add5(5));
 
 
-// // 🚀 This Keyword
-// console.log(this);
+// 🚀 This Keyword
+console.log(this);
 
-// var user = {
-//   firstName: "Hitesh",
-//   courseCount: 4,
-//   getCourseCount: function () {
-//     console.log("LINE 7", this);
-//     function sayHello() {
-//       console.log("Hello");
-//       console.log("LINE 10", this);
-//     }
-//     sayHello();
-//   },
-// };
+var user = {
+  firstName: "Hitesh",
+  courseCount: 4,
+  getCourseCount: function () {
+    console.log("LINE 7", this);
+    function sayHello() {
+      console.log("Hello");
+      console.log("LINE 10", this);
+    }
+    sayHello();
+  },
+};
 
-// user.getCourseCount();
+user.getCourseCount();
 
-// // 🚀
-// const uno = () => {
-//   return "I am One";
-// };
-
-// // const dos = () => {
-// //   setTimeout(() => {
-// //     return "I am two";
-// //   }, 3000);
-// // };
+// 🚀
+const uno = () => {
+  return "I am One";
+};
 
 // const dos = () => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve("I am two");
-//     }, 3000);
-//   });
+//   setTimeout(() => {
+//     return "I am two";
+//   }, 3000);
 // };
 
-// const tres = () => {
-//   return "I am Three";
-// };
+const dos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("I am two");
+    }, 3000);
+  });
+};
 
-// const callMe = async () => {
-//   let valOne = uno();
-//   console.log(valOne);
+const tres = () => {
+  return "I am Three";
+};
 
-//   let valTwo = await dos();
-//   console.log(valTwo);
+const callMe = async () => {
+  let valOne = uno();
+  console.log(valOne);
 
-//   let valThree = tres();
-//   console.log(valThree);
-// };
+  let valTwo = await dos();
+  console.log(valTwo);
 
-// callMe();
+  let valThree = tres();
+  console.log(valThree);
+};
+
+callMe();
+
+
+// 🚀 Celsius to fahrenheit conversion
+
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
