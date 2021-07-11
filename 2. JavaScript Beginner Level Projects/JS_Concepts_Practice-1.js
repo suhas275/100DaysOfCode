4ec7ad576bb6a8abb782c1df5c9b2c8abdd4618c
Suhas Gowda <67008179+suhas275@users.noreply.github.com>
2021-07-11 22:34:44 +0530

    Update

--- 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
+++ 2. JavaScript Beginner Level Projects/JS_Concepts_Practice-1.js
@@ -227,5 +227,6 @@ const celsius = prompt("Enter a celsius value: ");
 // calculate fahrenheit
 const fahrenheit = (celsius * 1.8) + 32
 
+
 // display the result
 console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
